d61aec0e9463dbf38801c45021ac964be502074d
Mohammad Aslam <aslampaasa420@gmail.com>
2026-07-25 16:50:36 +0530

    Creating Project Template + Auth Service

---
+++ auth-service/src/server.ts
@@ -0,0 +1,17 @@
+import app from './app.js';
+import { Config } from './config/index.js';
+import logger from './config/logger.js';
+
+const startServer = () => {
+  const PORT = Config.PORT;
+  try {
+    app.listen(PORT, () => {
+      logger.info('Server listening on port', { port: PORT });
+    });
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+};
+
+startServer();
