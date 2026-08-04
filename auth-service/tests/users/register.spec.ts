ef0dbb504d259c93a8412981bd819eb213756bc4
Mohammad Aslam <aslampaasa420@gmail.com>
2026-08-04 12:56:28 +0530

    User Registration - Testing for Valid JSON Response

--- auth-service/tests/users/register.spec.ts
+++ auth-service/tests/users/register.spec.ts
@@ -23,6 +23,26 @@ describe("POST /auth/register", () => {
             /* 3. Assert the result */
             expect(response.statusCode).toBe(201);
         })
+
+        it("should return valid JSON response", async () => {
+            /* 1. Arrange the data */
+            const userData = {
+                firstName: "Rakesh",
+                lastName: "Kumar",
+                email: "rakesh@mern.space",
+                password: "secret"
+            }
+
+            /* 2. Act on the data */
+            const response = await request(app)
+                .post("/auth/register")
+                .send(userData);
+
+            /* 3. Assert the result (application/json) */
+            expect((response.headers as Record<string, string>)['content-type']).toEqual(
+                expect.stringContaining("json")
+            );
+        })
     });
 
     describe("Fields are missing", () => {
