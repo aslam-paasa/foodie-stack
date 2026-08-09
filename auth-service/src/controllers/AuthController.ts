ffd9a64b5daa8d9030237b255502964ba2b4e098
Mohammad Aslam <aslampaasa420@gmail.com>
2026-08-09 13:55:42 +0530

    Fixed status code of Login Endpoint

--- auth-service/src/controllers/AuthController.ts
+++ auth-service/src/controllers/AuthController.ts
@@ -146,7 +146,7 @@ export class AuthController {
 
       /* Return the response (id) */
       this.logger.info('User has been logged in', { id: user.id });
-      res.status(201).json({ id: user.id });
+      res.status(200).json({ id: user.id });
     } catch (err) {
       next(err);
       return;
